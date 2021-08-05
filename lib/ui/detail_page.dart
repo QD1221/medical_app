1a0eda4fc34c097b418679a9168a5e0dc5c9d864
QD1221 <zxcvbnm12122000@gmail.com>
2021-08-05 22:03:27 +0700

    6th commit

---
+++ lib/ui/detail_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:medical_app/ui/home_page.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_back_ios_new_outlined,
+                        color: Colors.white,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      )),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 32,
+                  ),
+                  Column(
+                    children: [
+                      Text('Quang Tran'),
+                      Text('27 yo Allergy')
+                    ],
+                  )
+                ],
+              ),
+              flex: 3,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: Placeholder(),),
+                  Expanded(child: Placeholder(),),
+                ],
+              ),
+              flex: 6,
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                    return Container(
+                      
+                    );
+              }),
+              flex: 2,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(36),
+                  color: Colors.white.withOpacity(0.1)
+                ),
+              ),
+              flex: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/widget/card_widget.dart
+++ lib/ui/widget/card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medical_app/model/schedule.dart';
+import 'package:medical_app/ui/detail_page.dart';
 
 class CardWidget extends StatelessWidget {
   DoctorSchedule doctorSchedule;
@@ -7,107 +8,115 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 140,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 3,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  doctorSchedule.timeIndex ?? '-',
-                  style: TextStyle(color: Colors.white),
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => DetailPage()));
+      },
+      child: SizedBox(
+        height: 140,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    doctorSchedule.timeIndex ?? '-',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 10,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.05),
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(doctorSchedule.avatarImg!),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '${doctorSchedule.title}',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  SizedBox(
-                                    height: 4,
-                                  ),
-                                  Text(
-                                    '${doctorSchedule.subtitle}',
-                                    style: TextStyle(
-                                        color: Colors.white.withOpacity(0.2),
-                                        fontSize: 12),
-                                  ),
-                                ],
-                              ),
-                              doctorSchedule.isDone!
-                                  ? Container(
-                                      height: 32,
-                                      width: 32,
-                                      decoration: BoxDecoration(
-                                          color: Colors.greenAccent,
-                                          borderRadius:
-                                              BorderRadius.circular(6)),
-                                      child: Center(
-                                        child: Icon(
-                                          Icons.done,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    )
-                                  : Container(
-                                      height: 32,
-                                      width: 32,
-                                      decoration: BoxDecoration(
-                                          color: Colors.white.withOpacity(0.2),
-                                          borderRadius:
-                                              BorderRadius.circular(6)),
-                                    )
-                            ],
-                          ),
-                          Text(
-                            '${doctorSchedule.time}',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.2),
-                              fontSize: 12,
-                            ),
-                          )
-                        ],
-                      ),
+            Expanded(
+              flex: 10,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.05),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(doctorSchedule.avatarImg!),
                     ),
-                  )
-                ],
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '${doctorSchedule.title}',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 4,
+                                    ),
+                                    Text(
+                                      '${doctorSchedule.subtitle}',
+                                      style: TextStyle(
+                                          color: Colors.white.withOpacity(0.2),
+                                          fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                                doctorSchedule.isDone!
+                                    ? Container(
+                                        height: 32,
+                                        width: 32,
+                                        decoration: BoxDecoration(
+                                            color: Colors.greenAccent,
+                                            borderRadius:
+                                                BorderRadius.circular(6)),
+                                        child: Center(
+                                          child: Icon(
+                                            Icons.done,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      )
+                                    : Container(
+                                        height: 32,
+                                        width: 32,
+                                        decoration: BoxDecoration(
+                                            color:
+                                                Colors.white.withOpacity(0.2),
+                                            borderRadius:
+                                                BorderRadius.circular(6)),
+                                      )
+                              ],
+                            ),
+                            Text(
+                              '${doctorSchedule.time}',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.2),
+                                fontSize: 12,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
